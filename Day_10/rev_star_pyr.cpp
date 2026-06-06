b20c75912d43241e63272193b78cd66b008cd4a2
Kushagra Tripathi <kushagra2k6@gmail.com>
2026-06-06 15:24:04 +0530

    Day_10 uploaded.

---
+++ Day_10/rev_star_pyr.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=5;
+    for(int i=n;i>=1;i--){
+        for(int j=i;j<n;j++) cout<<" ";
+        for(int j=1;j<=(2*i-1);j++) cout<<"*";
+        cout<<"\n";
+    }
+    return 0;
+}
